345181469ab4b4cb6a4e6a8ef6552452a6dc3073
ofl <ofl@covered.jp>
2019-08-08 17:28:16 +0900

    エラークラスの追加 #16

---
+++ functions/src/errors.ts
@@ -0,0 +1,23 @@
+class ApplicationError implements Error {
+  public name = "ApplicationError";
+
+  constructor(public message: string) {}
+
+  toString() {
+    return this.name + ": " + this.message;
+  }
+}
+
+class UserDataLoadError extends ApplicationError {
+  public name = "UserDataLoadError";
+}
+
+class SentenceLoadError extends ApplicationError {
+  public name = "SentenceLoadError";
+}
+
+class EmptyAnswerError extends ApplicationError {
+  public name = "SentenceLoadError";
+}
+
+export { UserDataLoadError, SentenceLoadError, EmptyAnswerError };
