629c10d3c30832be09cfab25afef5d36fb5a23a9
ofl <ofl@covered.jp>
2019-08-10 14:12:11 +0900

    UserDataのコメント修正 #12

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,23 +60,4 @@ app.intent("Default Goodbye Intent", conv => {
   conv.close("Goodbye.");
 });
 
-// ユーザーが「Voice Match でアカウントに基づく情報を受け取る」場合
-// function loadUserDataFromStorage(conv: Conversation): UserData {
-//   const storage = conv.user.storage as UserData;
-//   if (
-//     typeof storage.repeatCount !== "undefined" &&
-//     typeof storage.lastReadUnixtime !== "undefined"
-//   ) {
-//     return storage;
-//   }
-
-//   const userData: UserData = {
-//     repeatCount: storage.repeatCount || 0,
-//     lastReadUnixtime: storage.lastReadUnixtime || new Date().getTime()
-//   };
-//   conv.user.storage = userData;
-
-//   return userData;
-// }
-
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
