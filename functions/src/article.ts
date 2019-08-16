f317dffcfed4b227a995cec86f93a2d22ddee649
ofl <ofl@covered.jp>
2019-08-16 14:50:55 +0900

    Article:getNext()、getNextOrIncrementCurrentIndex()などを追加した #37

--- functions/src/article.ts
+++ functions/src/article.ts
@@ -1,5 +1,6 @@
 import * as firebase from "firebase-admin";
 import { WhereFilterOp, Query } from "@google-cloud/firestore";
+import { AvailableArticleNotExist, ArticleNotFound } from "./errors";
 import Utils from "./utils";
 
 const firestore = firebase.firestore();
@@ -52,6 +53,44 @@ export default class Article {
     await batch.commit();
   }
 
+  static async getNext(): Promise<Article> {
+    const query = this.getQuery(Utils.getUnixtimeOfDaysBeforeNow(1));
+
+    return await this.load(query);
+  }
+
+  static async getNextOrIncrementCurrentIndex(
+    articleId: string,
+    currentSentence: string
+  ): Promise<Article> {
+    const currentArticle = await this.get(articleId);
+
+    const nextIndex = currentArticle.sentences.indexOf(currentSentence) + 1;
+    if (nextIndex > 0 && nextIndex < currentArticle.sentences.length) {
+      currentArticle.currentIndex = nextIndex;
+
+      return currentArticle;
+    }
+
+    const query = this.getQuery(currentArticle.unixtime, ">");
+
+    return await this.load(query);
+  }
+
+  static async get(articleId: string): Promise<Article> {
+    const doc = await firestore
+      .collection(ARTICLE_COLLECTION_PATH)
+      .doc(articleId)
+      .get();
+    const data = doc.data();
+
+    if (!data) {
+      throw new ArticleNotFound("Article not found");
+    } else {
+      return new this(data.guid, data.title, data.sentences, data.unixtime);
+    }
+  }
+
   static getQuery(unixtime: number, opStr: WhereFilterOp = ">="): Query {
     return firestore
       .collection(ARTICLE_COLLECTION_PATH)
@@ -59,6 +98,24 @@ export default class Article {
       .orderBy("unixtime");
   }
 
+  private static async load(query: Query): Promise<Article> {
+    const snapshot = await query.limit(1).get();
+    const articles: Article[] = await this.loadList(snapshot);
+
+    if (articles.length > 0) {
+      return articles[0];
+    } else {
+      throw new AvailableArticleNotExist("Article not exists");
+    }
+  }
+
+  private static async loadList(
+    snapshot: FirebaseFirestore.QuerySnapshot
+  ): Promise<Article[]> {
+    return snapshot.docs.map(doc => {
+      const data = doc.data();
+      return new this(data.guid, data.title, data.sentences, data.unixtime);
+    });
   }
 
   toObject(): Object {
@@ -68,5 +125,9 @@ export default class Article {
       sentences: this.sentences,
       unixtime: this.unixtime
     };
+  }
+
+  get currentSentence(): string {
+    return this.sentences[this.currentIndex];
   }
 }
