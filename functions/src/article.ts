1b1f6f29401e558b7c369282cbdeb944d9e5a895
ofl <ofl@covered.jp>
2019-08-16 14:49:24 +0900

    Articleコンストラクタにsentences、currentIndexを追加した #37

--- functions/src/article.ts
+++ functions/src/article.ts
@@ -6,21 +6,23 @@ const firestore = firebase.firestore();
 const ARTICLE_COLLECTION_PATH = "articles";
 
 export default class Article {
-  guid: string;
-  title: string;
-  sentences: string[];
-  unixtime: number;
+  readonly guid: string;
+  readonly title: string;
+  readonly sentences: string[];
+  readonly unixtime: number;
+  currentIndex: number;
 
   constructor(
     guid: string,
     title: string,
-    contentSnippet: string,
+    sentences: string[],
     isoDate: string
   ) {
     this.guid = guid;
     this.title = title;
-    this.sentences = Utils.textToSentences(contentSnippet);
+    this.sentences = sentences;
     this.unixtime = new Date(isoDate).getTime();
+    this.currentIndex = 0;
   }
 
   static async batchCreate(articles: Article[]): Promise<void> {
