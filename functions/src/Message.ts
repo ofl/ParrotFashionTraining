7e631a1daea743b4e4035610fa48751771bf4d98
ofl <ofl@covered.jp>
2019-09-22 17:33:29 +0900

    Messageクラスを追加した #9

---
+++ functions/src/Message.ts
@@ -0,0 +1,76 @@
+import Utils from "./utils";
+import AnswerResult from "./AnswerResult";
+
+class GreetingMessage {
+  static readonly WELCOME: string[] = ["Welcome to repeat after me."];
+  static readonly GOODBYE: string[] = ["Goodbye.", "Bye.", "See you again."];
+  static readonly YELL: string[] = [
+    "Let's start.",
+    "Let's begin.",
+    "Here we go."
+  ];
+}
+
+class RetryMessage {
+  static readonly AGAIN: string[] = ["Try again!."];
+}
+
+class ResultMessage {
+  static readonly EXCELLENT: string[] = ["Perfect!", "Excellent!"];
+  static readonly GOOD: string[] = ["OK!", "Good job!", "Well done!"];
+  static readonly REGRETTABLE: string[] = [
+    "So close!",
+    "Almost had it!",
+    "Well done!"
+  ];
+  static readonly POOR: string[] = [
+    "Don't mind!",
+    "Let's do your best next time!"
+  ];
+  static readonly SKIPPED: string[] = ["All right. Let's start next sentence."];
+}
+
+export default class Message {
+  static get welcome(): string {
+    return (
+      Utils.selectRandomly(GreetingMessage.WELCOME) +
+      Utils.selectRandomly(GreetingMessage.YELL)
+    );
+  }
+  static get bye(): string {
+    return Utils.selectRandomly(GreetingMessage.GOODBYE);
+  }
+  static get yell(): string {
+    return Utils.selectRandomly(GreetingMessage.YELL);
+  }
+  static get retry(): string {
+    return Utils.selectRandomly(RetryMessage.AGAIN);
+  }
+  static get resultExcellent(): string {
+    return Utils.selectRandomly(ResultMessage.EXCELLENT);
+  }
+  static get resultGood(): string {
+    return Utils.selectRandomly(ResultMessage.GOOD);
+  }
+  static get resultRegrettable(): string {
+    return Utils.selectRandomly(ResultMessage.REGRETTABLE);
+  }
+  static get resultPoor(): string {
+    return Utils.selectRandomly(ResultMessage.POOR);
+  }
+  static get resultSkipped(): string {
+    return Utils.selectRandomly(ResultMessage.SKIPPED);
+  }
+
+  static getResultMessage(result: AnswerResult): string {
+    if (result.isExcellent) {
+      return this.resultExcellent;
+    } else if (result.isGood) {
+      return this.resultGood;
+    } else if (result.isRegrettable) {
+      return this.resultRegrettable;
+    } else {
+      return this.resultPoor;
+    }
+  }
+}
