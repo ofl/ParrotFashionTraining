7d424120e92b10283f93d221ecbaa81cf92345ea
ofl <ofl@covered.jp>
2019-09-22 17:33:42 +0900

    SSMLクラスを追加した #9

---
+++ functions/src/SSML.ts
@@ -0,0 +1,13 @@
+export default class SSML {
+  static enclose(text: string): string {
+    return `<speak>${text}</speak>`;
+  }
+
+  static encloseSentence(sentence: string, rate: string = "normal"): string {
+    return `<s><prosody rate="${rate}">${sentence}<prosody></s>`;
+  }
+
+  static addBreak(time: number = 0.5): string {
+    return `<break time="${time}s">`;
+  }
+}
