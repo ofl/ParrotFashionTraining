8082a15f9ead794d30bf2fc9a1eab6981eff8678
ofl <ofl@covered.jp>
2019-10-06 16:33:06 +0900

    複数のニュースソースから記事を取得するように修正した #41

--- functions/src/batch.ts
+++ functions/src/batch.ts
@@ -1,13 +1,73 @@
 import Utils from "./Utils";
 import Crawler from "./Crawler";
+import TextSplitter from "./TextSplitter";
 import Article from "./Article";
 import * as moment from "moment";
 
+const SOURCES: string[] = [
+  "http://feeds.nytimes.com/nyt/rss/World",
+  "http://feeds.nytimes.com/nyt/rss/US",
+  "http://feeds.nytimes.com/nyt/rss/Business",
+  "http://feeds.nytimes.com/nyt/rss/Technology",
+  "http://feeds.nytimes.com/nyt/rss/Sports",
+  "http://feeds.nytimes.com/nyt/rss/Science",
+  "http://feeds.nytimes.com/nyt/rss/Health",
+  "http://feeds.nytimes.com/nyt/rss/Arts",
+  "http://feeds.bbci.co.uk/news/world/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.bbci.co.uk/news/business/rss.xml",
+  "http://feeds.bbci.co.uk/news/politics/rss.xml",
+  "http://feeds.bbci.co.uk/news/health/rss.xml",
+  "http://feeds.bbci.co.uk/news/education/rss.xml",
+  "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+  "http://feeds.bbci.co.uk/news/technology/rss.xml",
+  "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.bbci.co.uk/news/uk/rss.xml",
+  "http://feeds.reuters.com/news/artsculture",
+  "http://feeds.reuters.com/reuters/businessNews",
+  "http://feeds.reuters.com/reuters/companyNews",
+  "http://feeds.reuters.com/reuters/entertainment",
+  "http://feeds.reuters.com/reuters/environment",
+  "http://feeds.reuters.com/reuters/healthNews",
+  "http://feeds.reuters.com/reuters/lifestyle",
+  "http://feeds.reuters.com/news/wealth",
+  "http://feeds.reuters.com/reuters/oddlyEnoughNews",
+  "http://feeds.reuters.com/ReutersPictures",
+  "http://feeds.reuters.com/reuters/peopleNews",
+  "http://feeds.reuters.com/Reuters/PoliticsNews",
+  "http://feeds.reuters.com/reuters/scienceNews",
+  "http://feeds.reuters.com/reuters/sportsNews",
+  "http://feeds.reuters.com/reuters/technologyNews",
+  "http://feeds.reuters.com/reuters/topNews",
+  "http://feeds.reuters.com/Reuters/domesticNews",
+  "http://feeds.reuters.com/Reuters/worldNews"
+];
+
 export default class Batch {
   static async createArticlesFromRSS(days: number = 1) {
-    const articles = await Crawler.getFeedContents(
-      "http://feeds.nytimes.com/nyt/rss/Technology"
+    const articles: Article[] = [];
+
+    const contents = await Crawler.getFeedContents(
+      Utils.selectRandomly(SOURCES)
     );
+    contents.forEach(content => {
+      const sentences: string[] = TextSplitter.run(content.contentSnippet);
+      articles.push(
+        new Article(
+          content.guid,
+          content.title,
+          content.contentSnippet,
+          sentences,
+          Utils.maxWordCountInSentences(sentences),
+          content.creator,
+          content.isoDate
+        )
+      );
+    });
 
     const unixtime: number = moment()
       .add(-days, "day")
--- functions/src/crawler.ts
+++ functions/src/crawler.ts
@@ -1,13 +1,12 @@
 import Parser = require("rss-parser");
-import Article from "./Article";
-import TextSplitter from "./TextSplitter";
-import Utils from "./Utils";
 
 export default class Crawler {
-  static async getFeedContents(url: string): Promise<Article[]> {
+  static async getFeedContents(
+    url: string
+  ): Promise<{ [key: string]: string }[]> {
     const parser = new Parser();
     const feed = await parser.parseURL(url);
-    const result: Article[] = [];
+    const result: { [key: string]: string }[] = [];
 
     if (typeof feed.items === "undefined") {
       console.log("no articles exist");
@@ -19,21 +18,15 @@ export default class Crawler {
         typeof item.guid !== "undefined" &&
         typeof item.title !== "undefined" &&
         typeof item.contentSnippet !== "undefined" &&
-        typeof item.creator !== "undefined" &&
         typeof item.isoDate !== "undefined"
       ) {
-        const sentences: string[] = TextSplitter.run(item.contentSnippet);
-        result.push(
-          new Article(
-            item.guid,
-            item.title,
-            item.contentSnippet,
-            sentences,
-            Utils.maxWordCountInSentences(sentences),
-            item.creator,
-            item.isoDate
-          )
-        );
+        result.push({
+          guid: item.guid,
+          title: item.title,
+          contentSnippet: item.contentSnippet,
+          creator: item.creator || "",
+          isoDate: item.isoDate
+        });
       }
     });
 
