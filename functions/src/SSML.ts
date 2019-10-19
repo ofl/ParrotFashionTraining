b934f32489d7a537a722dc255627537727481879
ofl <ofl@covered.jp>
2019-10-19 20:53:31 +0900

    index.ts: Scenarioモデルの修正に伴い、シナリオ作成、ステータスの保存、会話を行うように修正 #86

--- functions/src/SSML.ts
+++ functions/src/SSML.ts
@@ -1,10 +1,14 @@
 export default class SSML {
   static enclose(text: string): string {
-    return `<speak>${text}</speak>`;
+    return `<speak><p>${text}</p></speak>`;
   }
 
-  static encloseSentence(sentence: string, rate: string): string {
-    return `<p><s><prosody rate="${rate}">${sentence}</prosody></s></p>`;
+  static encloseMessage(message: string): string {
+    return `<s>${message}</s>`;
+  }
+
+  static encloseRawText(questionText: string, rate: string): string {
+    return `<s><prosody rate="${rate}">${questionText}</prosody></s>`;
   }
 
   static addBreak(time: number = 0.5): string {
